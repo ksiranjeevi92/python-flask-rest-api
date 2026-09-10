e0c1cce4121008b9706fc62f32eaba556fd215c4
siranjeevi <ksiranjeevi92@gmail.com>
2026-09-10 23:04:49 +0530

    Add raise in fib

--- python-dsa/fib-list-solution.py
+++ python-dsa/fib-list-solution.py
@@ -1,5 +1,9 @@
 def fib(n):
     fib_list = [0,1]
+    
+    if n < 0:
+        raise ValueError("Invalid number")
+    
     for index in range(2,n+1):
         next_fib = fib_list[index - 1] + fib_list[index - 2]
         fib_list.append(next_fib)
