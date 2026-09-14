8a078f91be32df453071d5f3bef9d0e5e8a3f7ee
siranjeevi <ksiranjeevi92@gmail.com>
2026-09-14 13:16:30 +0530

    Completed the end points

--- schemas.py
+++ schemas.py
@@ -7,8 +7,12 @@ class PlainItemSchema(Schema):
     price = fields.Float(required=True)
 
 class PlainStoreSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
+
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
@@ -17,6 +21,18 @@ class ItemUpdateSchema(Schema):
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(lambda: PlainStoreSchema(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema(), dump_only=True))
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(lambda: PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema),dump_only=True)
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(lambda: PlainStoreSchema(), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema(), dump_only=True))
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
